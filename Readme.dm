f13ddd914e8d8d2122bc8986a56135c622ee88c1
cdup <cdup@MSI>
2020-07-16 22:58:47 +1200

    Add project files.

---
+++ Readme.dm
@@ -0,0 +1,33 @@
+************************************************************************************
+************************************************************************************
+
+	Goal of this program
+
+************************************************************************************
+
+************************************************************************************
+
+* Sort money in to groups of smaller bills and count the groups, for example 258 will be sorted like this:
+	
+	100: 2   50: 1  20: 0  10: 0  5: 1   2: 1    1: 1
+
+* Learn about "IF Statment" by using them to sort money in different textboxes.
+
+* Learn how to Cut down on repetitive code by using effective code logic (algorithms Design) for example 
+   using a for loop and array inorder to cut down 7 IF Statments to 1 IF Statment. 
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ SortMoney/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SortMoney
+{
+    public partial class SortMoney : Form
+    {
+        public SortMoney()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] sortMoney = { 100, 50, 20, 10, 5, 2, 1 };
+
+            TextBox textBox;
+
+            int j = 1; 
+
+            for (int i = 0; i < sortMoney.Length; i++)
+            {
+                // sort money
+                int results = Convert.ToInt32(textBox8.Text) / sortMoney[i];
+
+                //looping through the text boxes 
+                textBox = (TextBox)this.Controls["textBox" + j];
+
+                textBox.Text = results.ToString(); // display result in correct textboxes
+
+                if (results != 0) {
+                    //reduce the money from the textbox 
+                    int t = results * sortMoney[i];
+
+                    t = Convert.ToInt32(textBox8.Text) - t;
+
+                    textBox8.Text = t.ToString();
+                }
+                j++; // next textbox
+
+            }
+        }
+    }
+}
